432c225cc0dc8f618f11158dc66753d7d1338c88
JuanP539 <jpbarriosgarcia@gmail.com>
2024-10-11 13:25:07 -0300

    Front creado

---
+++ Angular/MovieRecomender/src/app/components/movie-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// movie-item.component.ts
+@Component({
+    standalone: true,
+    selector: 'movie-item',
+    templateUrl: './movie-item.component.html',
+    styleUrl: './movie-item.component.css',
+  })
+  export class TodoListItem {
+    /* Component behavior is defined in here */
+  }
